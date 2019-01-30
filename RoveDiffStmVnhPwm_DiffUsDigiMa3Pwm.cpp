bf85409ea0a4bdfede37ca38a132865d6faf66f7
JudahSchad <judahschad@gmail.com>
2019-01-30 13:23:24 -0600

    New folder structure per Energia cross library calling spec

---
+++ RoveDiffStmVnhPwm_DiffUsDigiMa3Pwm.cpp
@@ -0,0 +1,9 @@
+// Todo
+
+#include "RoveDiffStmVnhPwm_DiffUsDigiMa3Pwm.h"
+#include "RoveMotor.h"
+#include "RoveEncoder.h"
+
+#include <stdint.h>
+
+
--- src/RoveJoint.h
+++
@@ -1,6 +0,0 @@
-#ifndef _RoveJoint
-#define _RoveJoint
-
-#include "Differential/DiffStmVnhPwm_DiffUsDigiMa3Pwm.h"
-
-#endif
