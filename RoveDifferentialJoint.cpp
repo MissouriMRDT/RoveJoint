55d248a3c603975a9a01a08f3e1aabc7caf0b9ab
EliVerbrugge <eli.verbrugge@gmail.com>
2019-05-02 14:39:05 -0500

    Cleanup, added limit switch handling and modifed tilt/twist math

--- RoveDifferentialJoint.cpp
+++ RoveDifferentialJoint.cpp
@@ -1,7 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// MRDT 2019 => Energia Texas Instruments Tiva C, Todo
+// MRDT Differential Joint 2019
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 #include "RoveStmVnhPwm.h"
 #include "RoveUsDigiMa3Pwm.h"
 #include "RoveBoardMap.h"
@@ -13,25 +12,77 @@
 //////////////////////////////////////////////////////////////////////////////
 /////Scale our motor speeds so we can do a simultaneous twist and tilt////////
 //////////////////////////////////////////////////////////////////////////////
-void RoveDifferentialJoint::tiltTwistDecipercent( int tilt_decipercent, int twist_decipercent, bool right_compensation, bool left_compensation)
+void RoveDifferentialJoint::tiltTwistDecipercent( int tilt_decipercent, int twist_decipercent, comp_side compensation, float comp_factor)
 {
-  int left_tilt  = tilt_decipercent; // motors move in same direction to tilt on the pitch axis
-  int right_tilt = tilt_decipercent;
+  int left_speed  = tilt_decipercent - twist_decipercent;
+	int right_speed = tilt_decipercent + twist_decipercent;
 
-  int left_twist   = -twist_decipercent; // motors move in opposite direction to twist on the roll axis
-  int right_twist  =  twist_decipercent;
-
-  int left_speed  =( ( left_tilt + left_twist ) / 2 ) ;// combined differenterial
-  int right_speed = ( ( right_tilt + right_twist ) / 2 );
+	if(left_speed > 1000)
+	{
+	  right_speed = right_speed-(left_speed-1000);
+	  left_speed = 1000;
+	}
+	else if(left_speed < - 1000)
+	{
+	  right_speed = right_speed+(abs(left_speed)-1000);
+	  left_speed = -1000;
+	}
+	else if(right_speed > 1000)
+	{
+	  left_speed = left_speed-(right_speed-1000);
+	  right_speed = 1000;
+	}
+	else if(right_speed < - 1000)
+	{
+	  left_speed = left_speed+(abs(right_speed)-1000);
+	  right_speed = -1000;
+	}
   
-  if(right_compensation == true)
+  //compensation of motors, added because belt tensioning was unequal
+  if(compensation == Right)
   {
-    right_speed = (right_speed*190)/100;
+    right_speed = (right_speed*comp_factor);
   }
-  if(left_compensation == true)
+  if(compensation == Left)
   {
-    left_speed = (left_speed*190)/100;
+    left_speed = (left_speed*comp_factor);
   }
   RightMotor.drive(right_speed);
   LeftMotor.drive(left_speed);
+}
+
+void RoveDifferentialJoint::LowerLSPressed()
+{
+  //HIGH or LOW, but we can just map to a boolean
+  return digitalRead(LS_LOWER);
+}
+
+void RoveDifferentialJoint::UpperLSPressed()
+{
+  //HIGH or LOW, but we can just map to a boolean
+  return digitalRead(LS_UPPER);
+}
+
+bool RoveDifferentialJoint::atTiltLimit(int drive_speed)
+{
+  //if we are trying to move downwards, and we are hitting the lower limit switch stop
+  if(drive_speed < 0 && LowerLSPressed())
+  {
+    return true;
+  }
+  //if we are trying to move upwards, and we are hitting the upper limit switch stop
+  else if(drive_speed > 0 && UpperLSPressed())
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void RoveDifferentialJoint::attachLimitSwitches(uint8_t upperPin, uint8_t lowerPin)
+{
+    uint8_t LS_UPPER = upperPin;
+    uint8_t LS_LOWER = lowerPin;
 }
